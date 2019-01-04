4d451451892d032cb615c16a8a94481afbc840de
sdim-ua <ingersol@yandex.ua>
2019-01-04 15:03:58 +0300

    Create Solution.java

---
+++ Solution.java
@@ -0,0 +1,25 @@
+
+
+/* 
+Таблица умножения
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        //напишите тут ваш код
+        for (int i = 1; i < 11; i ++)
+        {
+                for (int j = 1; j < 11; j ++)
+                {
+                
+                       System.out.print(j * i + " ");
+                
+                }
+                System.out.println(" ");
+               
+        }
+      
+  
+    }
+    
+}
